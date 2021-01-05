626c38dc6a4867bd04d7d046601e50702c97e261
Simone Boerci <boercisimone@gmail.com>
2021-01-05 16:26:56 +0100

    Editable amount

--- lib/screens/calculator_screen.dart
+++ lib/screens/calculator_screen.dart
@@ -5,7 +5,7 @@ import '../models/mmc/percentage.dart';
 import '../models/mmc/percentage_sheet.dart';
 import '../models/managers/money_management_calculator.dart';
 
-class CalculatorScreen extends StatelessWidget{
+class CalculatorScreen extends StatefulWidget{
 
   final Percentage _commission = Percentage("Commission", 6, 5, 15);
   final Percentage _cashback = Percentage("Cashback", 50, 20, 50);
@@ -14,17 +14,53 @@ class CalculatorScreen extends StatelessWidget{
   final Percentage _investments = Percentage("Investments", 74, 50, 90);
 
   @override
+  _CalculatorScreenState createState() => _CalculatorScreenState(PercentageSheet(_commission, _cashback, _savings, _business, _investments));
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+
+  final TextEditingController _textEditingController = TextEditingController();
+
+  final PercentageSheet _percentageSheet;
+
+  MoneyManagementCalculator _mmc;
+  AssetList _assetList;
+
+  String _debugString = "";
+
+  _CalculatorScreenState(this._percentageSheet);
+
+  @override
+  void initState() {
+    if(_mmc == null)
+      _mmc = new MoneyManagementCalculator(_percentageSheet);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-
-    PercentageSheet _percentageSheet = PercentageSheet(_commission, _cashback, _savings, _business, _investments);
-    MoneyManagementCalculator _mmc = MoneyManagementCalculator(_percentageSheet);
-    AssetList _assetList = _mmc.run(1500);
-
-    String _debugString = "Main Card: ${_assetList.mainCard().money.toStringAsFixed(2)} \n Personal Card: ${_assetList.personalCard().money.toStringAsFixed(2)} \n Business Card: ${_assetList.businessCard().money.toStringAsFixed(2)} \n\n Investments Account: ${_assetList.investmentsAccount().money.toStringAsFixed(2)} \n Speculation Account: ${_assetList.speculationAccount().money.toStringAsFixed(2)} \n Savings Account: ${_assetList.savingsAccount().money.toStringAsFixed(2)} \n Bonus Account: ${_assetList.bonusAccount().money.toStringAsFixed(2)} \n\n Check: ${_assetList.assetValue().toStringAsFixed(2)}";
-
     return Scaffold(
-      body: Center(
-        child: Text(_debugString),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(_debugString),
+          SizedBox(height: 10,),
+          TextFormField(
+            controller: _textEditingController,
+            onFieldSubmitted: (text){
+              setState(() {
+                _assetList = _mmc.run(double.tryParse(text));
+                _debugString = "Main Card: ${_assetList.mainCard().money.toStringAsFixed(2)} \n Personal Card: ${_assetList.personalCard().money.toStringAsFixed(2)} \n Business Card: ${_assetList.businessCard().money.toStringAsFixed(2)} \n\n Investments Account: ${_assetList.investmentsAccount().money.toStringAsFixed(2)} \n Speculation Account: ${_assetList.speculationAccount().money.toStringAsFixed(2)} \n Savings Account: ${_assetList.savingsAccount().money.toStringAsFixed(2)} \n Bonus Account: ${_assetList.bonusAccount().money.toStringAsFixed(2)} \n\n Check: ${_assetList.assetValue().toStringAsFixed(2)}";
+              });
+            },
+          ),
+        ],
       ),
     );
   }
